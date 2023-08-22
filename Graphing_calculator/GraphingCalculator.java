a44609df968263cd8440782ab70dcda1ad891343
BPanoramic56 <panoramic56@proton.me>
2023-08-22 00:41:28 -0600

    Initial commit

---
+++ Graphing_calculator/GraphingCalculator.java
@@ -0,0 +1,14 @@
+package Graphing_calculator;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.JLabel;
+
+public class GraphingCalculator extends Frame{
+    
+    public GraphingCalculator(int width, int height){
+        setSize(width, height);
+    }
+
+}
---
+++ Graphing_calculator/main.java
@@ -0,0 +1,9 @@
+package Graphing_calculator;
+
+public class main{
+	public static void main(String[] args) {
+
+        GraphingCalculator calc = new GraphingCalculator(500, 6557);
+        calc.setVisible(true);
+    }
+}
