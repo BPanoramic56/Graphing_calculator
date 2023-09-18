a4c1608b6ff98a68ce5fdce9d2a2246aea9c3da0
BPanoramic56 <panoramic56@proton.me>
2023-09-17 23:45:31 -0600

    Minor changes to style and start of menu for personalization of graphs

--- Graphing_calculator/Calculator.java
+++ Graphing_calculator/Calculator.java
@@ -2,8 +2,7 @@ package Graphing_calculator;
 
 public class Calculator{
     public static void main(String[] args) {
-
-        GraphingCalculator calc = new GraphingCalculator(5000, 5000);
+        GraphingCalculator calc = new GraphingCalculator(5000, 5000, 10, 10);
         calc.setVisible(true);
     }
 }
--- Graphing_calculator/GraphingCalculator.java
+++ Graphing_calculator/GraphingCalculator.java
@@ -1,30 +1,44 @@
 package Graphing_calculator;
-
 import java.awt.*;
 import javax.swing.JFrame;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class GraphingCalculator extends JFrame{
+public class GraphingCalculator extends JFrame {
 
-    public GraphingCalculator(int width, int height){
+    private double scale;
+    private double res;
+    
+    public GraphingCalculator(int width, int height, double scale, double res){
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(width, height);
+        this.res    = res;
+        this.scale  = scale;
         add("Center", new createCanvas());
+    }
+
+    public double getRes(){
+        return this.res;
+    }
+    public double getScale(){
+        return this.scale;
     }
 
     class createCanvas extends Canvas{
 
         private int maxX;
         private int maxY;
-        private double res;
         private double scale;
+        private double res;
+        private boolean[] decision;
 
         public void paint(Graphics g){
             Dimension d = this.getSize();
             this.maxX   = d.width - 1;
             this.maxY   = d.height - 1;
-            this.res    = 1000;
-            this.scale = 100;
+            this.res    = getRes();
+            this.scale  = getScale();
+            menu();
             long start = System.nanoTime();
             drawGraph(g);
             g.translate(this.maxX / 2, this.maxY / 2);
@@ -32,7 +46,25 @@ public class GraphingCalculator extends JFrame{
             System.out.println("Process Time: " + (System.nanoTime() - start)/1_000_000 + "ms");
         }
 
-        public void drawGraph(Graphics g){
+        private void menu(){
+            boolean[] decision = new boolean[3];
+            Scanner option = new Scanner(System.in);
+            int chosen;
+            while (true){
+                System.out.println("ENTER - Save and exit");
+                System.out.println("1 - Draw Points:    \t[" + (decision[0] == true ? "true" : "false") + "]");
+                System.out.println("2 - Draw Lines:     \t[" + (decision[1] == true ? "true" : "false") + "]");
+                System.out.println("3 - Draw Rectangles:\t[" + (decision[2] == true ? "true" : "false") + "]");
+                chosen = option.nextInt();
+                if (chosen == 0)
+                    break;
+                decision[chosen - 1] = !decision[chosen-1];
+            }
+            option.close();
+            this.decision = decision;
+        }
+
+        private void drawGraph(Graphics g){
             int midX = this.maxX/2;
             int midY = this.maxY/2;
             g.setColor(Color.BLACK);
@@ -43,46 +75,48 @@ public class GraphingCalculator extends JFrame{
                 g.drawArc(midX-i, midY-i, i*2, i*2, 0, 360);
         }
 
-        public void calculatePoints(Graphics g){
+        private void calculatePoints(Graphics g){
             ArrayList<Point> yPoints = new ArrayList<Point>();
             for (double x = -(this.maxX/2); x < this.maxX; x+= 0.1/this.res){
                 double y = -(
-                    Math.sin(x) * Math.sin(x/2) * x
+                    Math.sin(Math.cos(x))
                 );
                 yPoints.add(new Point(x * scale, y * scale * 10));
             }
             drawResults(yPoints, g);
         }
         
-        public void drawResults(ArrayList<Point> yPoints, Graphics g){   
-            g.setColor(Color.BLACK);   
-            drawPoints(yPoints, g);
-            g.setColor(Color.RED);
-            drawLines(yPoints, g);
-            g.setColor(Color.GRAY);
-            // drawRect(yPoints, g);
+        private void drawResults(ArrayList<Point> yPoints, Graphics g){  
+            if (decision[0]) 
+                drawPoints(yPoints, g);
+            if (decision[1])
+                drawLines(yPoints, g);
+            if (decision[2])
+                drawRect(yPoints, g);
         }
 
-        public void drawPoints(ArrayList<Point> yPoints, Graphics g){
+        private void drawPoints(ArrayList<Point> yPoints, Graphics g){
+            g.setColor(Color.BLACK);   
             for (int i = 1; i < yPoints.size(); i++){ 
-                int circleSize = 10;
+                int circleSize = 3;
                 g.drawArc((int) yPoints.get(i).getX()-(circleSize/2), (int) yPoints.get(i).getY()-(circleSize/2), circleSize, circleSize, 0, 360);
             }
         }
 
-        public void drawLines(ArrayList<Point> yPoints, Graphics g){
+        private void drawLines(ArrayList<Point> yPoints, Graphics g){
+            g.setColor(Color.RED);
             for (int i = 1; i < yPoints.size(); i++){ 
                 g.drawLine((int) yPoints.get(i-1).getX(), (int) yPoints.get(i-1).getY(), (int) yPoints.get(i).getX(), (int) yPoints.get(i).getY());
             }
         }
 
-        public void drawRect(ArrayList<Point> yPoints, Graphics g){
+        private void drawRect(ArrayList<Point> yPoints, Graphics g){
+            g.setColor(Color.GRAY);
             for (int i = 1; i < yPoints.size(); i++){ 
                 double pointOneX = yPoints.get(i-1).getX();
                 double pointOneY = yPoints.get(i-1).getY();
-                double pointTwoX = yPoints.get(i).getX();
                 double height = yPoints.get(i-1).getY();
-                double width = bigger(pointOneX, pointTwoX) - smaller(pointOneX, pointTwoX);
+                double width = bigger(pointOneX, yPoints.get(i).getX()) - smaller(pointOneX, yPoints.get(i).getX());
                 if (pointOneY < 0){
                     height = -height;
                 }
@@ -93,7 +127,6 @@ public class GraphingCalculator extends JFrame{
                 g.drawRect((int) pointOneX, (int) (pointOneY - 1), (int) width, (int) height);
             }
         }
-
 
         private double bigger(double num1, double num2){
             return num1 >= num2 ? num1 : num2;
